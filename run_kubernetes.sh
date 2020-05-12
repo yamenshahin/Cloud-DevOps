5c5b197ac90519267effdf2c931a28b36a912770
EC2 Default User <yamenshahin2gmail.com>
2020-05-12 12:59:03 +0000

    kubernetes run

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -5,14 +5,16 @@
 # Step 1:
 # This is your Docker ID/path
 # dockerpath=<>
+dockerpath=yamenshahin/cloud-devops
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl run app --image=yamenshahin/cloud-devops:project4 --port=80
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl expose deployment app --type=LoadBalancer --port=8080 --target-port=80
--- upload_docker.sh
+++ upload_docker.sh
@@ -6,10 +6,14 @@
 # Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
+dockerpath=yamenshahin/cloud-devops
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+docker login --username yamenshahin
+docker tag app yamenshahin/cloud-devops:project4
 
 # Step 3:
 # Push image to a docker repository
+docker push $dockerpath
